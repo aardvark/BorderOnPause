51d44c1c2bfd687344b99df88e777a35e91802b3
aardvark <e.a.latuhin@gmail.com>
2019-05-03 19:46:45 +0300

    Cleanup AssemblyInfo
    Signed-off-by: aardvark <e.a.latuhin@gmail.com>

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,31 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using BorderOnPause;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BorderOnPause")]
+[assembly: AssemblyTitle(Mod.Name)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("BorderOnPause")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1D285F44-66CD-47AA-B1DA-0A1C4109D840")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyProduct("RimWorld Mod")]
 [assembly: AssemblyVersion("0.0.*")]
 [assembly: AssemblyFileVersion("0.0.*")]
