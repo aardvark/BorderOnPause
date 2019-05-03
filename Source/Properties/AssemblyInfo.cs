7e0955aa18ead1e9653058c2302c96963b61d2d5
aardvark <e.a.latuhin@gmail.com>
2019-05-03 19:48:22 +0300

    Clean up assembly info
    Signed-off-by: aardvark <e.a.latuhin@gmail.com>

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using BorderOnPause;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("RimWorld Mod")]
-[assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("0.0.*")]
+[assembly: AssemblyVersion(Mod.Version)]
+[assembly: AssemblyFileVersion("1.0.*")]
